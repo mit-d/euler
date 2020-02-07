d53e6002e0a2696a82a4275e729d41a9021d1a85
mit-d <derekmttn@gmail.com>
2020-02-07 16:19:16 -0700

    added solution to 2

---
+++ 2.py
@@ -0,0 +1,13 @@
+def compute(n):
+    sum = int(0)
+    a = int(1)
+    b = int(2)
+    while(a < n):
+        a, b = b, a+b
+        if(a%2 == 0):
+            sum += a
+    return sum
+
+if __name__ == '__main__':
+    print(compute(int(4000000)))
+
