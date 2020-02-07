f6b27e2bfb61325f2fd0a5471f62625dfe7d90f3
mit-d <derekmttn@gmail.com>
2020-02-07 16:45:08 -0700

    added is_palindrome

--- 4.py
+++ 4.py
@@ -2,4 +2,13 @@
 # The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
 # Find the largest palindrome made from the product of two 3-digit numbers.
 
+import math
+
+def is_palindrome(n):
+    s = str(n)
+    l = s[:math.floor(len(s)/2)]
+    r = s[math.ceil(len(s)/2):]
+    if l == r[::-1]:
+        return True
+    return False
 
