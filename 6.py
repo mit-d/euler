f0debe624d9701d45b176713ec2a5913f4f79bf6
mit-d <derekmttn@gmail.com>
2020-02-10 10:15:29 -0700

    added solution for 6

---
+++ 6.py
@@ -0,0 +1,23 @@
+# Sum Square Difference
+
+# sum of squares 1**2 + ...... + n**2
+# squares of sum (1 + ....... + n)**2
+
+# we want (sum of sq - sq of sum)
+
+def SumOfSquares(number):
+    ans = 0
+    while number > 0:
+        ans += number ** 2
+        number -= 1
+    return ans
+def SquareOfSums(number):
+    ans = 0
+    while number > 0:
+        ans += number
+        number -= 1
+    return ans ** 2
+
+if __name__ == '__main__':
+    n = 100
+    print(SquareOfSums(n) - SumOfSquares(n))
