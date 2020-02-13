fabbe4472c274aef04e537a7dcda493c3ca2932b
mit-d <derekmttn@gmail.com>
2020-02-12 19:18:38 -0700

    added input and solution for 11

--- 11.py
+++ 11.py
@@ -1,77 +1,28 @@
 # Greatest product of 4 in a line on grid of numbers
 import sys
-
-
-def horizntl_product(matrix, adjacent, r, c):
-    print("h")
-    if r > len(matrix) or c > (len(matrix[r]) - adjacent):
-        print("%")
-        return 0
-    else:
-        product = 1
-        for offset in range(adjacent):
-            product *= int(matrix[r + offset][c])
-        return product
-
-
-def vertical_product(matrix, adjacent, r, c):
-    print("v")
-    if r > len(matrix):
-        return 0
-    else:
-        product = 1
-        for offset in range(adjacent):
-            product *= int(matrix[r][c + offset])
-        return product
-
-
-def diagonal_product_right(matrix, adjacent, r, c):
-    print("r")
-    if r > len(matrix) or c > (len(matrix[r]) - adjacent):
-        print("%")
-        return 0
-    else:
-        product = 1
-        for offset in range(adjacent):
-            product *= int(matrix[r + offset][c + offset])
-        return product
-
-
-def diagonal_product_left(matrix, adjacent, r, c):
-    if c < adjacent:
-        print("*")
-        return 0
-    else:
-        product = 1
-        for offset in range(adjacent):
-            product *= int(matrix[r - offset][c - offset])
-        return product
-
-
-def largest_product(matrix, adjacent, r, c):
-    return max(
-        [
-            horizntl_product(matrix, adjacent, r, c),
-            vertical_product(matrix, adjacent, r, c),
-            diagonal_product_right(matrix, adjacent, r, c),
-            diagonal_product_left(matrix, adjacent, r, c),
-        ]
-    )
+import numpy as np
+from functools import reduce
+from operator import mul
 
 
 def read_int_matrix():
     arr = []
     for line in sys.stdin:
-        arr.append(line.split())
+        arr.append(list(map(int, line.split())))
     return arr
 
 
 if __name__ == "__main__":
-    adjacent_numbers = int(4)
+    adj = int(4)
     m = read_int_matrix()
-    all = []
-    for row_i in range(0, len(m) + 1 - adjacent_numbers):
-        for col_i in range(0, len(m[row_i])):
-            all.append(largest_product(m, adjacent_numbers, row_i, col_i))
-    print(len(all))
-    print(max(all))
+    mm = np.array(m)
+    ans = set()
+    for row in range(len(m)):
+        for col in range(len(m[row])):
+            box = mm[row : row + adj, col : col + adj]
+            vecs = box.diagonal(), box[0, :], box[:, 0], box[::-1].diagonal()
+            sums = box.diagonal(), box[0, :], box[:, 0], box[::-1].diagonal()
+            q = [reduce(mul, j, 1) for j in vecs]
+            for i in q:
+                ans.add(i)
+    print(max(ans))
